181d73c0b596621c05c5b8612c4bbd7ae1d0b43b
Enock Kimutai Sigei <esigei091@gmail.com>
2025-03-08 17:45:03 +0300

    api

--- cookshare/src/components/services/api.js
+++ cookshare/src/components/services/api.js
@@ -1,12 +1,5 @@
-// import axios from "axios";
-
-// export const api = axios.create({
-//   baseURL: "http://localhost:8080/api",
-// });
-
-
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://cookshare-hub.onrender.com/api", 
+  baseURL: "https://cookshare-hub.onrender.com/api", 
 });
